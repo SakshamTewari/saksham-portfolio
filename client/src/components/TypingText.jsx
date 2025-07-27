db723032c6314bad7eff603dd25a7c18f5d3745d
Saksham Tewari <sakshamtewari3@gmail.com>
2025-07-27 16:59:30 +0530

    add: Animated Typing Text component

---
+++ client/src/components/TypingText.jsx
@@ -0,0 +1,11 @@
+const TypingText = ({ text }) => {
+    return (
+        <>
+            <div className="hero-top-left typing-container ">
+                <p className='typing-loop'>{text}</p>
+            </div>
+        </>
+    )
+}
+
+export default TypingText;
